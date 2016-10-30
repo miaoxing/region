2537e3d0e57f0f391e86f894f31a082b4ee9c5d6
twinh <twinhuang@qq.com>
2016-10-30 21:24:58 +0800

    迁移增加数据 #1828

--- src/Migration/V20161020173530CreateRegionsTable.php
+++ src/Migration/V20161020173530CreateRegionsTable.php
@@ -4,24 +4,29 @@ namespace Miaoxing\Region\Migration;
 
 use miaoxing\plugin\BaseService;
 use services\Scheme;
+use Wei\Db;
 
 /**
  * @property Scheme $scheme
+ * @property Db $db
  */
 class V20161020173530CreateRegionsTable extends BaseService
 {
     public function up()
     {
-        $this->scheme->table('regions2')
+        $this->scheme->table('regions')
             ->id()
             ->int('parentId')
             ->string('name', 32)
             ->tinyInt('sort', 2)->comment('顺序,从大到小排列')
             ->exec();
+
+        //$file = dirname(dirname(__DIR__)) . '/resources/schemas/regions.sql';
+        //$this->db->executeUpdate(file_get_contents($file));
     }
 
     public function down()
     {
-        $this->scheme->dropIfExists('regions2');
+        $this->scheme->dropIfExists('regions');
     }
 }
