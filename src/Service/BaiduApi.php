46fb82a993e9683cb548b61fd64caf2338497adb
twinh <twinhuang@qq.com>
2016-11-17 14:47:41 +0800

    lbs相关服务加入到region中 #2028 移除sina, taobao api, key改为配置等

---
+++ src/Service/BaiduApi.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Miaoxing\Region\Service;
+
+use miaoxing\plugin\BaseService;
+
+/**
+ * 百度Web服务API
+ *
+ * @method \Wei\Http http($options)
+ * @link http://developer.baidu.com/map/webservice.htm
+ */
+class BaiduApi extends BaseService
+{
+    /**
+     * 在lbs云官网注册的access key，作为访问的依据
+     *
+     * @var string
+     */
+    protected $accessKey;
+
+    /**
+     * 浏览器端的access key
+     *
+     * @var string
+     */
+    protected $browserAccessKey;
+
+    /**
+     * 根据IP获取地址信息
+     *
+     * 参考错误
+     * Array ( [status] => 2 [message] => Request Parameter Error:ak required/参数必须 [uid] => [sk] => [logformat] => [ak] => )
+     *
+     * @param string $ip
+     * @return array
+     * @link http://developer.baidu.com/map/ip-location-api.htm
+     */
+    public function getIpInfo($ip)
+    {
+        $response = $this->http([
+            'url' => 'http://api.map.baidu.com/location/ip',
+            'referer' => true,
+            'dataType' => 'json',
+            'timeout' => 30000,
+            'throwException' => false,
+            'data' => [
+                'ak' => $this->accessKey,
+                'ip' => $ip,
+            ],
+        ]);
+
+        if ($response['status'] !== 0) {
+            $this->logger->warning('获取IP信息失败', [
+                'ip' => $ip,
+                'res' => $response->getResponse(),
+            ]);
+        }
+
+        // 重新构造数据结构,和其他接口保持一致
+        return [
+            'code' => $response['status'] === 0 ? 1 : -$response['status'],
+            'province' => $response['content']['address_detail']['province'],
+            'city' => $response['content']['address_detail']['city'],
+            'district' => $response['content']['address_detail']['district'],
+        ];
+    }
+
+    public function getAccessKey()
+    {
+        return $this->accessKey;
+    }
+
+    public function getBrowserAccessKey()
+    {
+        return $this->browserAccessKey;
+    }
+}
---
+++ src/Service/Lbs.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Miaoxing\Region\Service;
+
+use miaoxing\plugin\BaseService;
+
+/**
+ * Location-based service
+ *
+ * @property \Wei\Request $request
+ * @property \Wei\Cache $cache
+ * @property \Wei\Logger $logger
+ * @property BaiduApi $ipApi
+ */
+class Lbs extends BaseService
+{
+    protected $providers = [
+        'ipApi' => 'baiduApi',
+    ];
+
+    /**
+     * 根据IP获取详细信息
+     *
+     * @param null|string $ip
+     * @return array
+     */
+    public function getIpInfo($ip = null)
+    {
+        $ip || $ip = $this->request->getIp();
+        try {
+            $that = $this;
+            $info = $this->cache->get('ipInfo' . $ip, function () use ($ip, $that) {
+                return $that->ipApi->getIpInfo($ip);
+            });
+        } catch (\ErrorException $e) {
+            $info = [
+                'code' => -1,
+                'province' => '',
+                'city' => '',
+                'district' => ''
+            ];
+            $this->logger->warning($e, ['ip' => $ip]);
+        }
+        return $info;
+    }
+
+    /**
+     * @param string|null $ip
+     * @return array
+     */
+    public function getIpInfoWithShortName($ip = null)
+    {
+        $info = $this->getIpInfo($ip);
+        $info['province'] = $this->removeSuffix($info['province'], ['省', '自治区']);
+        $info['city'] = $this->removeSuffix($info['city'], ['市', '区']);
+        return $info;
+    }
+
+    protected function removeSuffix($name, $suffixes)
+    {
+        foreach ($suffixes as $suffix) {
+            if ($this->wei->isEndsWith($name, $suffix)) {
+                return substr($name, 0, - strlen($suffix));
+            }
+        }
+        return $name;
+    }
+}
