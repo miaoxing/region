e50fb84c656606ac57298b02671ff2f177f31290
twinh <twinhuang@qq.com>
2018-10-24 23:00:54 +0800

    feat: parentId支持传入文本

--- src/Controller/Areas.php
+++ src/Controller/Areas.php
@@ -10,11 +10,18 @@ class Areas extends \Miaoxing\Plugin\BaseController
 
     public function indexAction($req)
     {
+        // 支持编号和名称两种参数
+        if (!is_numeric($req['parentId'])) {
+            $parentId = wei()->appDb('areas')->select('id')->fetchColumn(['name' => $req['parentId']]);
+        } else {
+            $parentId = $req['parentId'];
+        }
+
         // 构造基本的查询
         $areas = wei()->appDb('areas')
             ->select('id AS value, name AS label')
             ->asc('id')
-            ->andWhere(['parentId' => (int) $req['parentId']]);
+            ->andWhere(['parentId' => (int) $parentId]);
 
         // 排除指定的区域
         if ($req['exceptIds'] && is_array($req['exceptIds'])) {
