475c08fc5459deabd94d20bef81b77877f5a2f78
twinh <twinhuang@qq.com>
2021-01-22 12:48:59 +0800

    refactor: 同步插件核心和模型的更改

--- src/Metadata/RegionTrait.php
+++ src/Metadata/RegionTrait.php
@@ -2,33 +2,15 @@
 
 namespace Miaoxing\Region\Metadata;
 
-use Miaoxing\Plugin\Model\ModelTrait;
-
 /**
- * RegionTrait
- *
  * @property int $id
  * @property int $parentId
  * @property string $name
  * @property string $shortName
- * @property int $sort 顺序,从大到小排列
+ * @property int $sort 顺序，从大到小排列
  * @property bool $hasChildren
  * @internal will change in the future
  */
 trait RegionTrait
 {
-    use ModelTrait;
-
-    /**
-     * @var array
-     * @see CastTrait::$casts
-     */
-    protected $casts = [
-        'id' => 'int',
-        'parent_id' => 'int',
-        'name' => 'string',
-        'short_name' => 'string',
-        'sort' => 'int',
-        'has_children' => 'bool',
-    ];
 }
--- src/Service/RegionModel.php
+++ src/Service/RegionModel.php
@@ -3,11 +3,13 @@
 namespace Miaoxing\Region\Service;
 
 use Miaoxing\Plugin\BaseModel;
+use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Service\Model;
 use Miaoxing\Region\Metadata\RegionTrait;
 
 class RegionModel extends BaseModel
 {
+    use ModelTrait;
     use RegionTrait;
 
     public function getShortNameAttribute()
